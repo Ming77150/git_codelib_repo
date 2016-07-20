859f35d190a729ea9e623a20b728593f9d718623
Ming Dong <md0619@outlook.com>
2016-07-20 23:55:47 +0800

    new deply batch file for rcks

---
+++ deploy for rcks.bat
@@ -0,0 +1,33 @@
+ECHO deploying external package...
+
+SET srcpath=.
+SET dstpath=..\external
+
+MD %dstpath%
+MD %dstpath%\include
+
+MD %dstpath%\vs2015\windt\x86\debug\include
+MD %dstpath%\vs2015\windt\x86\debug\bin
+MD %dstpath%\vs2015\windt\x86\debug\lib
+
+MD %dstpath%\vs2015\windt\x86\release\include
+MD %dstpath%\vs2015\windt\x86\release\bin
+MD %dstpath%\vs2015\windt\x86\release\lib
+
+MD %dstpath%\vs2015\windt\x64\debug\include
+MD %dstpath%\vs2015\windt\x64\debug\bin
+MD %dstpath%\vs2015\windt\x64\debug\lib
+
+MD %dstpath%\vs2015\windt\x64\release\include
+MD %dstpath%\vs2015\windt\x64\release\bin
+MD %dstpath%\vs2015\windt\x64\release\lib
+
+ECHO deploying asio module...
+XCOPY %srcpath%\asio\deploy\asio-1.10.6\*.*							%dstpath%								/S /E
+
+ECHO deploying rapidjson module...
+XCOPY %srcpath%\rapidjson\deploy\rapidjson-1.0.2\*.*				%dstpath%								/S /E
+
+ECHO deploying rapidxml module...
+XCOPY %srcpath%\rapidxml\deploy\rapidxml-1.13\*.*					%dstpath%								/S /E
+
