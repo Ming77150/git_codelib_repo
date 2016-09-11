b1ca316d25d725d9e100e0b238e04af671ad4184
Ming Dong <md0619@outlook.com>
2016-09-11 23:13:12 +0800

    remove support for x86 add support for vs2012

--- deploy for rcks.bat
+++ deploy for rcks.bat
@@ -6,13 +6,13 @@ SET dstpath=..\external
 MD %dstpath%
 MD %dstpath%\include
 
-MD %dstpath%\vs2015\windt\x86\debug\include
-MD %dstpath%\vs2015\windt\x86\debug\bin
-MD %dstpath%\vs2015\windt\x86\debug\lib
+REM MD %dstpath%\vs2015\windt\x86\debug\include
+REM MD %dstpath%\vs2015\windt\x86\debug\bin
+REM MD %dstpath%\vs2015\windt\x86\debug\lib
 
-MD %dstpath%\vs2015\windt\x86\release\include
-MD %dstpath%\vs2015\windt\x86\release\bin
-MD %dstpath%\vs2015\windt\x86\release\lib
+REM MD %dstpath%\vs2015\windt\x86\release\include
+REM MD %dstpath%\vs2015\windt\x86\release\bin
+REM MD %dstpath%\vs2015\windt\x86\release\lib
 
 MD %dstpath%\vs2015\windt\x64\debug\include
 MD %dstpath%\vs2015\windt\x64\debug\bin
@@ -21,6 +21,14 @@ MD %dstpath%\vs2015\windt\x64\debug\lib
 MD %dstpath%\vs2015\windt\x64\release\include
 MD %dstpath%\vs2015\windt\x64\release\bin
 MD %dstpath%\vs2015\windt\x64\release\lib
+
+MD %dstpath%\vs2012\windt\x64\debug_2012\include
+MD %dstpath%\vs2012\windt\x64\debug_2012\bin
+MD %dstpath%\vs2012\windt\x64\debug_2012\lib
+
+MD %dstpath%\vs2012\windt\x64\release_2012\include
+MD %dstpath%\vs2012\windt\x64\release_2012\bin
+MD %dstpath%\vs2012\windt\x64\release_2012\lib
 
 ECHO deploying asio module...
 XCOPY %srcpath%\asio\deploy\asio-1.10.6\*.*							%dstpath%								/S /E
